947ff1bf7a0a03e8bac8891186c6fa72b7ab1c3c
Tri Sutrisno <mas.trisutrisno@gmail.com>
2021-06-18 16:58:48 +0700

    add button style

--- src/components/CameraScanner.vue
+++ src/components/CameraScanner.vue
@@ -1,20 +1,31 @@
 <template>
   <div class="scanner-container">
-      <div v-show="!isLoading">
-          <div>
-            <button class="button" id="startButton" @click="onStart">Start</button>
-            <button class="button" id="resetButton" @click="onReset">Reset</button>
-          </div>
-          <video poster="data:image/gif, AAAA" id="video" ref="scanner" />
-          <div class="overlay"></div>
-          <div class="laser"></div>
-          <div id="sourceSelectPanel" style="display: none">
-            <label for="sourceSelect">Change video source:</label>
-            <select id="sourceSelect" style="max-width: 400px" v-model="selectedDeviceId">
-                <option v-for="source in sourceSelect" :key="source.value" :value="source.value"> {{source.text}} </option>
-            </select>
-          </div>
+    <div v-show="!isLoading">
+      <div class="button-container">
+        <button class="button" id="startButton" @click="onStart">Start</button>
+        <button class="button" id="resetButton" @click="onReset">Reset</button>
       </div>
+      <video poster="data:image/gif, AAAA" id="video" ref="scanner" />
+      <div class="overlay"></div>
+      <div class="laser"></div>
+
+      <div class="select" id="sourceSelectPanel" style="display: none">
+        <label for="sourceSelect">Change video source:</label>
+        <select
+          id="sourceSelect"
+          style="max-width: 400px"
+          v-model="selectedDeviceId"
+        >
+          <option
+            v-for="source in sourceSelect"
+            :key="source.value"
+            :value="source.value"
+          >
+            {{ source.text }}
+          </option>
+        </select>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -28,12 +39,15 @@ export default {
       isLoading: false,
       scanner: new BrowserMultiFormatReader(),
       isMediaStreamAPISupported:
-                navigator &&
-                navigator.mediaDevices &&
-                'enumerateDevices' in navigator.mediaDevices,
+        navigator &&
+        navigator.mediaDevices &&
+        'enumerateDevices' in navigator.mediaDevices,
       sourceSelect: [],
       selectedDeviceId: ''
     }
+  },
+  watch: {
+    selectedDeviceId: 'onStart'
   },
   mounted () {
     if (!this.isMediaStreamAPISupported) {
@@ -41,7 +55,7 @@ export default {
       return
     }
     this.start()
-    this.$refs.scanner.oncanplay = event => {
+    this.$refs.scanner.oncanplay = (event) => {
       this.isLoading = false
       this.$emit('loaded')
     }
@@ -75,7 +89,7 @@ export default {
         (result, err) => {
           if (result) {
             this.$emit('decode', result.text)
-            console.log(result.text)
+            // console.log(result.text)
           }
           if (err && !(err instanceof Exception)) {
             console.error(err)
@@ -90,67 +104,72 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 video {
-    max-width: 100%;
-    max-height: 100%;
+  max-width: 100%;
+  max-height: 100%;
+}
+.button-container {
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5em;
 }
 .scanner-container {
-    position: relative;
+  position: relative;
 }
 .overlay-element {
-    position: absolute;
-    top: 0;
-    width: 100%;
-    height: 99%;
-    background: rgba(30, 30, 30, 0.5);
-    -webkit-clip-path: polygon(
-        0% 0%,
-        0% 100%,
-        20% 100%,
-        20% 20%,
-        80% 20%,
-        80% 80%,
-        20% 80%,
-        20% 100%,
-        100% 100%,
-        100% 0%
-    );
-    clip-path: polygon(
-        0% 0%,
-        0% 100%,
-        20% 100%,
-        20% 20%,
-        80% 20%,
-        80% 80%,
-        20% 80%,
-        20% 100%,
-        100% 100%,
-        100% 0%
-    );
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 99%;
+  background: rgba(30, 30, 30, 0.5);
+  -webkit-clip-path: polygon(
+    0% 0%,
+    0% 100%,
+    20% 100%,
+    20% 20%,
+    80% 20%,
+    80% 80%,
+    20% 80%,
+    20% 100%,
+    100% 100%,
+    100% 0%
+  );
+  clip-path: polygon(
+    0% 0%,
+    0% 100%,
+    20% 100%,
+    20% 20%,
+    80% 20%,
+    80% 80%,
+    20% 80%,
+    20% 100%,
+    100% 100%,
+    100% 0%
+  );
 }
 .laser {
-    width: 60%;
-    margin-left: 20%;
-    background-color: tomato;
-    height: 1px;
-    position: absolute;
-    top: 40%;
-    z-index: 2;
-    box-shadow: 0 0 4px red;
-    -webkit-animation: scanning 2s infinite;
-    animation: scanning 2s infinite;
+  width: 60%;
+  margin-left: 20%;
+  background-color: tomato;
+  height: 1px;
+  position: absolute;
+  top: 40%;
+  z-index: 2;
+  box-shadow: 0 0 4px red;
+  -webkit-animation: scanning 2s infinite;
+  animation: scanning 2s infinite;
 }
 @-webkit-keyframes scanning {
-    50% {
-        -webkit-transform: translateY(75px);
-        transform: translateY(75px);
-    }
+  50% {
+    -webkit-transform: translateY(75px);
+    transform: translateY(75px);
+  }
 }
 @keyframes scanning {
-    50% {
-        -webkit-transform: translateY(75px);
-        transform: translateY(75px);
-    }
+  50% {
+    -webkit-transform: translateY(75px);
+    transform: translateY(75px);
+  }
 }
 </style>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import 'webrtc-adapter';
+// import 'webrtc-adapter'
 import CameraScanner from './components/CameraScanner'
 
-export default{
-  install:function(Vue){
-    Vue.component('v-zxing',CameraScanner)
+export default {
+  install: function (Vue) {
+    Vue.component('v-zxing', CameraScanner)
   }
 }
